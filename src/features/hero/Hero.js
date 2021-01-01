f40d64824dd4c2a23c6d58b3f2b5e6aaf3be14d0
dim <jimstef@outlook.com>
2021-01-01 23:21:02 +0200

    fixed bugs

--- src/features/hero/Hero.js
+++ src/features/hero/Hero.js
@@ -1,12 +1,17 @@
+import VideoCall from '../../flat/illustrations/VideoCall';
 import './hero.css'
 
 function Hero(){
   return(
     <div className="hero-container">
       <div className="hero-text">
+        <img src="https://www.drempathy.com/wp-content/uploads/2020/09/drempathy-logo-png.png" alt="Logo"/>
         <h1 style={{color:'white', fontWeight:'bold', fontSize:'3em'}}>DrEmpathy</h1>
         <p style={{textAlign:'left', fontSize:'1.2em', color:'white'}}>Live therapy. You talk. We listen.</p>
-        </div>
+      </div>
+      <div style={{display:'flex', justifyContent:'center'}}>
+        <VideoCall className="hero-illustration"/>
+      </div>
     </div>
   )
 }
---
+++ src/flat/ourTherapistItem.js/OurTherapistItem.js
@@ -0,0 +1,3 @@
+function OurTherapistItem(therapist){
+
+}
