c3c84b790bba4ba6574883590d904ebe53267344
dim <jimstef@outlook.com>
2021-01-29 00:45:10 +0200

    added drawer

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,13 +1,17 @@
 import {useState, useEffect} from 'react';
 import {useSelector} from 'react-redux';
-import { UserOutlined, HomeOutlined, CalendarOutlined } from '@ant-design/icons';
+import { Drawer } from 'antd';
+import { UserOutlined, HomeOutlined, CalendarOutlined, MenuOutlined } from '@ant-design/icons';
 import {Link, useHistory} from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import './header.css';
 
 function Header(){
   const [scroll, setScroll] = useState(0);
+  const [drawerVisible, setDrawerVisible] = useState(false);
   const {token} = useSelector(state=>state.authentication);
   const history = useHistory();
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
 
   // if we are on landing page the background is "dark" and the header icons should be white
   let darkBackground = false;
@@ -26,32 +30,41 @@ function Header(){
 
   function handleProfileClick(){
     history.push(token?'/me':'/pre_login');
+    onClose()
   }
 
   function handleBlogClick(){
     history.push('/blogs');
+    onClose()
   }
 
   function handleHomeClick(){
     history.push('/');
+    onClose()
   }
 
   function handleSessionClick(){
     history.push('/my_sessions');
+    onClose()
   }
 
   function handleForTherapistsClick(){
     history.push('/pre_register');
+    onClose()
   }
 
   function handleTherapistClick(){
     history.push('/therapists');
+    onClose()
   }
   
   function scrollListener(e){
     setScroll(window.scrollY)
   }
 
+  function onClose(){
+    setDrawerVisible(false);
+  }
   useEffect(()=>{
     window.addEventListener('scroll', scrollListener);
     
@@ -61,40 +74,81 @@ function Header(){
   },[])
 
   return(
+    
     <div className={`header ${darkBackground?'header-dark':'header-light'}`} 
     style={{backgroundColor:transparentHeader?'transparent':null}}>
       <div style={{flex:1, display:'flex'}}>
-      <IconWrapper onClick={handleHomeClick} darkBackground={darkBackground}>
-        <HomeOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>
-      </IconWrapper>
+        <IconWrapper onClick={handleHomeClick} darkBackground={darkBackground}>
+          <HomeOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>
+        </IconWrapper>
         
       </div>
-      <IconWrapper onClick={handleBlogClick} darkBackground={darkBackground}>
-        {/*<UserOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
-        <p style={{margin: 0, fontWeight:'bold'}}>Blog</p>
-      </IconWrapper>
-      {token?
-      <IconWrapper onClick={handleSessionClick} darkBackground={darkBackground}>
-        {/*<CalendarOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
-        <p style={{margin: 0, fontWeight:'bold'}}>Συνεδρίες</p>
-      </IconWrapper>
-      :null}
-      {token?
-      <IconWrapper onClick={handleTherapistClick} darkBackground={darkBackground}>
-        <p style={{margin: 0, fontWeight:'bold'}}>Θεραπευτές</p>
-      </IconWrapper>
-      :null}
-      <IconWrapper onClick={handleProfileClick} darkBackground={darkBackground}>
-        {/*<UserOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
-        <p style={{margin: 0, fontWeight:'bold'}}>{token?'Το προφίλ μου':'Συνδέσου'}</p>
-      </IconWrapper>
-      {token?
-      <IconWrapper onClick={handleForTherapistsClick} darkBackground={darkBackground}>
-        {/*<CalendarOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
-        <p style={{margin: 0, fontWeight:'bold'}}>Για επαγγελματίες</p>
-      </IconWrapper>
-      :null}
+
+      {isMobile?(
+        <>
+        <IconWrapper darkBackground={darkBackground} onClick={()=>setDrawerVisible(true)}>
+          <MenuOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>
+        </IconWrapper>
+        <Drawer
+          title={'Drempathy'}
+          placement='left'
+          closable={true}
+          onClose={onClose}
+          visible={drawerVisible}
+          key='left'
+        >
+          <IconDrawerWrapper onClick={handleBlogClick} darkBackground={darkBackground}>
+          <p style={{margin: 0, fontWeight:'bold'}}>Blog</p>
+          </IconDrawerWrapper>
+          {token?
+            <IconDrawerWrapper onClick={handleSessionClick} darkBackground={darkBackground}>
+              <p style={{margin: 0, fontWeight:'bold'}}>Συνεδρίες</p>
+            </IconDrawerWrapper>
+            :null}
+          {token?
+            <IconDrawerWrapper onClick={handleTherapistClick} darkBackground={darkBackground}>
+              <p style={{margin: 0, fontWeight:'bold'}}>Θεραπευτές</p>
+            </IconDrawerWrapper>
+            :null}
+          <IconDrawerWrapper onClick={handleProfileClick} darkBackground={darkBackground}>
+            <p style={{margin: 0, fontWeight:'bold'}}>{token?'Το προφίλ μου':'Συνδέσου'}</p>
+          </IconDrawerWrapper>
+          {token?
+            <IconDrawerWrapper onClick={handleForTherapistsClick} darkBackground={darkBackground}>
+            <p style={{margin: 0, fontWeight:'bold'}}>Για επαγγελματίες</p>
+          </IconDrawerWrapper>:null}
+        </Drawer>
+        </>
+        ):(
+        <>
+        <IconWrapper onClick={handleBlogClick} darkBackground={darkBackground}>
+          {/*<UserOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
+          <p style={{margin: 0, fontWeight:'bold'}}>Blog</p>
+        </IconWrapper>
+        {token?
+          <IconWrapper onClick={handleSessionClick} darkBackground={darkBackground}>
+            {/*<CalendarOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
+            <p style={{margin: 0, fontWeight:'bold'}}>Συνεδρίες</p>
+          </IconWrapper>
+          :null}
+        {token?
+          <IconWrapper onClick={handleTherapistClick} darkBackground={darkBackground}>
+            <p style={{margin: 0, fontWeight:'bold'}}>Θεραπευτές</p>
+          </IconWrapper>
+          :null}
+        <IconWrapper onClick={handleProfileClick} darkBackground={darkBackground}>
+          {/*<UserOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
+          <p style={{margin: 0, fontWeight:'bold'}}>{token?'Το προφίλ μου':'Συνδέσου'}</p>
+        </IconWrapper>
+        {token?
+          <IconWrapper onClick={handleForTherapistsClick} darkBackground={darkBackground}>
+            {/*<CalendarOutlined style={{fontSize: 24, color:darkBackground?'white':'black'}}/>*/}
+            <p style={{margin: 0, fontWeight:'bold'}}>Για επαγγελματίες</p>
+          </IconWrapper>:null}
+        </>
+      )}
     </div>
+    
   )
 }
 
@@ -107,4 +161,11 @@ function IconWrapper({children, darkBackground, onClick}){
   )
 }
 
+function IconDrawerWrapper({children, darkBackground, onClick}){
+  return(
+    <div onClick={onClick} role="button" style={{cursor:'pointer', margin:10, color:'black', fontSize:'1.2rem'}}>
+      {children}
+    </div>
+  )
+}
 export default Header;
